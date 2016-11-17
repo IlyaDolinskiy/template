e92e46e6243d269ffa8456b6b90af7e6a614876d
Roman Kuznetsov <r.kuznetsow@gmail.com>
2016-11-18 00:48:16 +0300

    Added JsonCpp and PugiXml

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,63 @@
 #include <QSurfaceFormat>
 #include "main_window.hpp"
 
+#include <json/json.h>
+#include <json/writer.h>
+#include <fstream>
+#include <iostream>
+
+void WriteJson()
+{
+  Json::Value settings;
+  Json::Value resolutions(Json::arrayValue);
+  resolutions.append(Json::Value("800x600"));
+  resolutions.append(Json::Value("1024x768"));
+  resolutions.append(Json::Value("1280x1024"));
+
+  auto & root = settings["settings"];
+  root["resolution"] = resolutions;
+  root["aliensCount"] = 100;
+  root["bulletsCount"] = 200;
+
+  root["entities"]["gun"]["health"] = 50;
+  root["entities"]["alien"]["health"] = 20;
+  root["entities"]["obstacle"]["health"] = 15;
+
+  std::ofstream settingsFile;
+  settingsFile.open("settings.json");
+  if (settingsFile.is_open())
+  {
+    Json::StyledWriter styledWriter;
+    settingsFile << styledWriter.write(settings);
+    settingsFile.close();
+  }
+}
+
+void ReadJson()
+{
+  Json::Value settings;
+  std::ifstream file("settings.json");
+  if (file.is_open())
+  {
+    file >> settings;
+    file.close();
+  }
+
+  Json::Value & resolutions = settings["settings"]["resolution"];
+  if (resolutions.isArray())
+  {
+    for (Json::Value::ArrayIndex i = 0; i < resolutions.size(); i++)
+      std::cout << resolutions[i].asString() << std::endl;
+  }
+  std::cout << settings["settings"]["aliensCount"].asInt() << std::endl;
+  std::cout << settings["settings"]["bulletsCount"].asInt() << std::endl;
+
+  Json::Value & entities = settings["settings"]["entities"];
+  std::cout << entities["gun"]["health"].asInt() << std::endl;
+  std::cout << entities["alien"]["health"].asInt() << std::endl;
+  std::cout << entities["obstacle"]["health"].asInt() << std::endl;
+}
+
 int main(int argc, char ** argv)
 {
   QApplication a(argc, argv);
